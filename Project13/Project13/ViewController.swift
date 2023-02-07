ac4b6084f3e4633c1d8d3ebf875363a407a92f38
Kiny <89192734912nik@gmail.com>
2023-02-07 21:19:52 +0300

    added main

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
     }
-
-
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
 }
 
