ad9c7499602c3b44ddb1176fb23007633b032cb2
Kiny <89192734912nik@gmail.com>
2023-02-07 20:57:03 +0300

    init

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by nikita on 07.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
