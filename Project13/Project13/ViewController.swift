204cae139cb5ce3d2e61c24a1f6be7242495afd5
Kiny <89192734912nik@gmail.com>
2023-02-09 02:20:32 +0300

    day 54

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
+    @IBOutlet var changeFilterButton: UIButton!
+    @IBOutlet var radius: UISlider!
+    
+    
     var currentImage: UIImage!
     
     var context: CIContext!
@@ -24,8 +28,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         context = CIContext()
         currentFilter = CIFilter(name: "CISepiaTone")
+        changeFilterButton.setTitle("Change filter:\nCISepiaTone", for: .normal)
+        
         
     }
+    
     
     @objc func importPicture() {
         let picker = UIImagePickerController()
@@ -41,6 +48,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    @IBAction func radiusChanged(_ sender: Any) {
         applyProcessing()
     }
     
@@ -72,14 +82,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        changeFilterButton.setTitle("Change filter:\n\(actionTitle)", for: .normal)
+        
+        
 
         applyProcessing()
     }
     
     @IBAction func save(_ sender: Any) {
-        guard let image = imageView.image else { return }
+        guard let image = imageView.image else {
+            let ac = UIAlertController(title: "No image found", message: "You can't save the void", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default))
+            
+            present(ac, animated: true)
+            return
+        }
         
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
@@ -91,6 +112,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
         if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(radius.value * 200, forKey: kCIInputRadiusKey) }
         if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
         if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
         
